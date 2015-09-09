b8336f6ce2dd93f8341b0e6883787c83e263242b
nvcleemp <nico.vancleemput@gmail.com>
2015-09-09 20:33:55 +0200

    Boolean type

---
+++ boolean.h
@@ -0,0 +1,17 @@
+/*
+ * Main developer: Nico Van Cleemput
+ *
+ * Copyright (C) 2015 Nico Van Cleemput.
+ * Licensed under the GNU AFFERO GPL, read the file LICENSE for details.
+ */
+
+#ifndef BOOLEAN_H
+#define	BOOLEAN_H
+
+typedef int boolean;
+
+#define TRUE 1
+#define FALSE 0
+
+#endif	/* BOOLEAN_H */
+
